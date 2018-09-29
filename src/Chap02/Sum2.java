53280091a0af8084a9cc3a427a8ac7bc686a38c7
danlan <qas87@icloud.com>
2018-09-29 19:02:57 +0800

    18.9.29修改

---
+++ src/Chap02/Sum2.java
@@ -0,0 +1,10 @@
+//求两个整数值57和32的和并显示结果
+package Chap02;
+
+public class Sum2 {
+    public static void main(String[] srgs) {
+        //(57 + 32)括号内优先执行
+        //"字符串+数值"或"数值+字符串"，会先将数值转换为字符串再进行拼接
+        System.out.println("57 + 32 = " + (57 + 32));
+    }
+}
---
+++ src/Chap02/Sum3.java
@@ -0,0 +1,9 @@
+//求两个整数值57和32的和并显示结果（错误）
+package Chap02;
+
+public class Sum3 {
+    public static void main(String[] args) {
+        //" + 57 + 32"首先将数值转换为字符串57和32然后输出5732
+        System.out.println("57 + 32 = " + 57 + 32);
+    }
+}
