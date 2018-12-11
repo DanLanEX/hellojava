cebf42a7cdef8b5aa5f6273742de959e983d4506
danlan <qas87@icloud.com>
2018-12-11 15:34:54 +0800

    18.12.11提交

---
+++ src/Chap07/oooHomeWork7_13.java
@@ -0,0 +1,8 @@
+package Chap07;
+
+import java.util.Scanner;
+
+//练习：编写方法set、reset、inverse，分别返回将整数x的第pos位（从最低位开始0,1,2,...）设为1后的值、设为0后的值、取反后的值。
+public class oooHomeWork7_13 {
+
+}
---
+++ src/Chap07/oooHomeWork7_14.java
@@ -0,0 +1,4 @@
+package Chap07;
+//练习：编写方法setN、resetN、inverseN，分别返回将整数x的第pos位开始的n位设为1后的值、设为0后的值，取反后的值。
+public class oooHomeWork7_14 {
+}
